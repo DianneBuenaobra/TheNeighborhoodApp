d8aa37bf57138e313f6a2aa9f646a9f7e574a1b1
DianneBuenaobra <Buenaobra.258124@balagtas.sti.edu.ph>
2024-01-11 00:14:08 +0800

    dianne1

--- TheNeighborhoodApp/FrmAdminCalendar.Designer.cs
+++ TheNeighborhoodApp/FrmAdminCalendar.Designer.cs
@@ -71,7 +71,7 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(479, 288);
+            this.panel1.Size = new System.Drawing.Size(479, 273);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -79,7 +79,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Location = new System.Drawing.Point(19, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 20);
             this.label1.TabIndex = 0;
@@ -88,7 +88,7 @@
             // txtTitle
             // 
             this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTitle.Location = new System.Drawing.Point(23, 38);
+            this.txtTitle.Location = new System.Drawing.Point(23, 29);
             this.txtTitle.Name = "txtTitle";
             this.txtTitle.Size = new System.Drawing.Size(424, 22);
             this.txtTitle.TabIndex = 2;
@@ -96,7 +96,7 @@
             // dtPicker1
             // 
             this.dtPicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtPicker1.Location = new System.Drawing.Point(23, 95);
+            this.dtPicker1.Location = new System.Drawing.Point(23, 86);
             this.dtPicker1.Name = "dtPicker1";
             this.dtPicker1.Size = new System.Drawing.Size(200, 21);
             this.dtPicker1.TabIndex = 3;
@@ -106,7 +106,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(19, 72);
+            this.label2.Location = new System.Drawing.Point(19, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 20);
             this.label2.TabIndex = 1;
@@ -117,7 +117,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(229, 95);
+            this.label3.Location = new System.Drawing.Point(229, 86);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(15, 20);
             this.label3.TabIndex = 4;
@@ -126,7 +126,7 @@
             // dtPicker2
             // 
             this.dtPicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtPicker2.Location = new System.Drawing.Point(247, 95);
+            this.dtPicker2.Location = new System.Drawing.Point(247, 86);
             this.dtPicker2.Name = "dtPicker2";
             this.dtPicker2.Size = new System.Drawing.Size(200, 21);
             this.dtPicker2.TabIndex = 6;
@@ -136,7 +136,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(19, 128);
+            this.label4.Location = new System.Drawing.Point(19, 119);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 20);
             this.label4.TabIndex = 7;
@@ -145,7 +145,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(23, 151);
+            this.textBox1.Location = new System.Drawing.Point(23, 142);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(424, 87);
@@ -156,9 +156,9 @@
             this.panel2.BackColor = System.Drawing.Color.SteelBlue;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(12, 306);
+            this.panel2.Location = new System.Drawing.Point(12, 291);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(479, 337);
+            this.panel2.Size = new System.Drawing.Size(479, 352);
             this.panel2.TabIndex = 9;
             // 
             // panel3
@@ -175,7 +175,7 @@
             this.panel3.Controls.Add(this.label6);
             this.panel3.Location = new System.Drawing.Point(2, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(469, 326);
+            this.panel3.Size = new System.Drawing.Size(469, 342);
             this.panel3.TabIndex = 0;
             // 
             // label6
@@ -264,7 +264,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(311, 249);
+            this.button1.Location = new System.Drawing.Point(311, 240);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 9;
@@ -273,7 +273,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(391, 249);
+            this.button2.Location = new System.Drawing.Point(391, 240);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 10;
@@ -283,9 +283,10 @@
             // DateContainer
             // 
             this.DateContainer.BackColor = System.Drawing.Color.SteelBlue;
-            this.DateContainer.Location = new System.Drawing.Point(5, 45);
+            this.DateContainer.Location = new System.Drawing.Point(6, 43);
+            this.DateContainer.Margin = new System.Windows.Forms.Padding(1);
             this.DateContainer.Name = "DateContainer";
-            this.DateContainer.Size = new System.Drawing.Size(459, 277);
+            this.DateContainer.Size = new System.Drawing.Size(457, 296);
             this.DateContainer.TabIndex = 17;
             // 
             // month
--- TheNeighborhoodApp/FrmAdminCalendar.cs
+++ TheNeighborhoodApp/FrmAdminCalendar.cs
@@ -26,13 +26,14 @@ namespace TheNeighborhoodApp
             
             DateTime now = DateTime.Now;
             
-            DateTime startofMonth = new DateTime(now.Year, 1, now.Month);
+            DateTime startofMonth = new DateTime(now.Year, now.Month, 1);
             int totalDays = DateTime.DaysInMonth(now.Year, now.Month);
             int startoftheWeek = Convert.ToInt32(startofMonth.DayOfWeek.ToString("d")) + 1;
-            DateTime monthToday = new DateTime(now.Month);
-            month.Text = monthToday.ToString();
+            int monthdigit = now.Month;
+            
             for (int i = 1; i < startoftheWeek; i++)
             {
+                
                 UserControlBlank blank = new UserControlBlank();
                 DateContainer.Controls.Add(blank);
             }
@@ -41,6 +42,15 @@ namespace TheNeighborhoodApp
                 UserControlDays days = new UserControlDays();
                 days.Dates(i);
                 DateContainer.Controls.Add(days);
+            }
+            switch (monthdigit)
+            {
+                case 1: month.Text = "January";break;case 2:month.Text = "February";break;
+                case 3:month.Text = "March";break;case 4:month.Text = "April";break;
+                case 5:month.Text = "May";break;case 6:month.Text = "June";break;
+                case 7:month.Text = "July";break;case 8:month.Text = "August";break;
+                case 9:month.Text = "September";break;case 10:month.Text = "October";break;
+                case 11:month.Text = "November";break;case 12:month.Text = "December";break;
             }
 
 
